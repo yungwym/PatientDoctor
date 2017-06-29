0d5a5d0a8fe9adefea6769c0ccbee8edbaabdeb9
Rob Wymer <wymer1120@gmail.com>
2017-06-29 12:30:54 -0400

    First Commit

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by Alex Wymer  on 2017-06-29.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
